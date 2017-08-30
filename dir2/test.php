313f27555a2588e617f1f6691140d920e35f98ab
Lukasz Jedrzejewski <lukasso137@wp.pl>
2017-08-30 22:51:22 +0200

    another change

---
+++ dir2/test.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang ="pl>
+<head>
+	<meta charset="utf-8"/>
+</head>
+
+<body>
+
+<?php
+ 
+ $imie = "Łukasz!";
+ 
+ echo "Hello World!<br /><br />";
+ 
+ echo "$imie, bądź dobrym programistą i developerem!<br /><br />";
+ 
+ echo $imie.", dasz radę z programowaniem - osoby mniej zdolne i mniej entuzjastyczne, które miały mniejszą potrzebę bycia programistami, dali radę, a skoro oni dali radę, to i Ty dasz radę!<br />
+Uwierz mi, poradzimy sobie, w tej może najtrudniejszej drodze, ale się nie poddawaj, a codziennie pracuj nad rozwijaniem aplikacji, nad kodem, na próbach zrozumienia i aplikowania logiki biznesowej, nie zaniedbując przy tym frontendu!<br /><br />
+Trzeba pracować naprawdę dużo (code every day), i coraz bardziej się skupiać, ale robić sobie przerwy - dzięki temu, będziemy coraz szybsi w tym, a to jest ważne, żeby nie zamulać przy kompie, a stawać sie coraz bardziej produktywnym i efektywnym, więc proszę, pracuj nad ukończeniem kursu podstawowego php, tym szybciej przejdziemy do budowania aplikacji w Laravel, którą teraz kupiliśmy, a potem przechodzimy do JavaScripu podstawowego > frameworków jak jQuery.<br /><br /><br /><br />";
+
+echo "Lukasso, be good at programming, increase your development skills by every day training with coding and building applications! <br /><br />";
+
+echo "$imie, be good programmer, please! <br /><br />";
+
+echo $imie.", don't give up with it - work every day on yourself, do 3 hours trening at least with programming, and do not forget about phisical exercises, so you will be better in every way, month by month ;)<br /><br />";
+
+echo $imie.", to dzielny zawodnik, miał czas osłabienia, ale teraz powstanie z popiołów i zostanie dobrym programistą i graplerem, oraz znajdzie dobrą pracę z dobrymi zarobkami, gdzie będzie się dobrze czuł i dobrze rozwijał, co m.in. przełozy się na szczęśce i dobrobyt wszelaki dla niego i jego rodziny! Amen!";
+
+
+ 
+ 
+
+?>
+
+</body>
+</html>
